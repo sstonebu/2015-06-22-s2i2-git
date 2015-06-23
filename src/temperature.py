707d1a87a3219451f2ff4b6e36028414c530efcb
Sam Stoneburner <sstonebu@umn.edu>
2015-06-23 09:47:25 -0400

    working temp conversion

--- src/temperature.py
+++ src/temperature.py
@@ -5,8 +5,9 @@
 """
 
 def f_to_k(temp):
-    converted = ((temp - 32) * (5 / 9)) + 273.15
+    converted = ((temp - 32) * (5 / 9.0)) + 273.15
     return converted
 
 
+print(f_to_k(32))
 print(f_to_k(212))
