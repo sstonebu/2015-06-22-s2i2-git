e2b9c95e5c166844ecab5fc7b69fadc62d81de40
John Gomez <gomez.john.a@gmail.com>
2015-06-23 14:27:30 -0400

    Added the python script heyscript.py that requests user input and prints the input string in reverse order. Run using 'python heyscript.py'

---
+++ heyscript.py
@@ -0,0 +1,6 @@
+#this is a python script that reads input from the command line and then returns the word backwards
+
+a = raw_input("Please type a word and press enter: ")
+
+print("Here is youre word reversed: ")
+print a[::-1]
